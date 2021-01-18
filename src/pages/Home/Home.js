a64cc9b4175bb57c7d842b60a631a41d7410760d
saputroandi <saputro.andi593@gmail.com>
2021-01-18 17:22:49 +0700

    resize bottom bar

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -115,35 +115,35 @@ export default function Home(){
                 {/* end product */}
 
                 {/* mobile bottom navigation */}
-                <div className="fixed w-full py-3 bg-gradient-to-br from-blue-500 to-blue-400 bottom-0 lg:hidden">
+                <div className="fixed w-full py-2 bg-gradient-to-br from-blue-500 to-blue-400 bottom-0 lg:hidden">
                     <div className="flex flex-row justify-between px-6 mx-4">
                         <div className="flex-col">
-                            <button className="opacity-60 rounded-xl focus:outline-none hover:bg-blue-700 focus:ring-1 focus:ring-blue-600 focus:opacity-100 focus:shadow-none shadow-lg p-2">
+                            <button className="opacity-60 rounded-xl focus:outline-none hover:bg-blue-700 focus:ring-1 focus:ring-blue-600 focus:opacity-100 focus:shadow-none shadow-lg p-1">
                                 <div className="flex justify-center">
-                                    <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path></svg>
+                                    <svg className="w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path></svg>
                                 </div>
                                 <div>
-                                    <p>Menu</p>
+                                    <p className="font-light text-xs">Menu</p>
                                 </div>
                             </button>
                         </div>
                         <div className="flex-col">
-                            <button className="opacity-60 rounded-xl focus:outline-none hover:bg-blue-700 focus:ring-1 focus:ring-blue-600 focus:opacity-100 focus:shadow-none shadow-lg p-2">
+                            <button className="opacity-60 rounded-xl focus:outline-none hover:bg-blue-700 focus:ring-1 focus:ring-blue-600 focus:opacity-100 focus:shadow-none shadow-lg p-1">
                                 <div className="flex justify-center">
-                                    <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z" clipRule="evenodd"></path></svg>
+                                    <svg className="w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z" clipRule="evenodd"></path></svg>
                                 </div>
                                 <div>
-                                    <p>Profile</p>
+                                    <p className="font-light text-xs">Profile</p>
                                 </div>
                             </button>
                         </div>
                         <div className="flex-col">
-                            <button className="opacity-60 rounded-xl focus:outline-none hover:bg-blue-700 focus:ring-1 focus:ring-blue-600 focus:opacity-100 focus:shadow-none shadow-lg p-2">
+                            <button className="opacity-60 rounded-xl focus:outline-none hover:bg-blue-700 focus:ring-1 focus:ring-blue-600 focus:opacity-100 focus:shadow-none shadow-lg p-1">
                                 <div className="flex justify-center">
-                                    <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M3 1a1 1 0 000 2h1.22l.305 1.222a.997.997 0 00.01.042l1.358 5.43-.893.892C3.74 11.846 4.632 14 6.414 14H15a1 1 0 000-2H6.414l1-1H14a1 1 0 00.894-.553l3-6A1 1 0 0017 3H6.28l-.31-1.243A1 1 0 005 1H3zM16 16.5a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0zM6.5 18a1.5 1.5 0 100-3 1.5 1.5 0 000 3z"></path></svg>
+                                    <svg className="w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M3 1a1 1 0 000 2h1.22l.305 1.222a.997.997 0 00.01.042l1.358 5.43-.893.892C3.74 11.846 4.632 14 6.414 14H15a1 1 0 000-2H6.414l1-1H14a1 1 0 00.894-.553l3-6A1 1 0 0017 3H6.28l-.31-1.243A1 1 0 005 1H3zM16 16.5a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0zM6.5 18a1.5 1.5 0 100-3 1.5 1.5 0 000 3z"></path></svg>
                                 </div>
                                 <div>
-                                    <p>Cart</p>
+                                    <p className="font-light text-xs">Cart</p>
                                 </div>
                             </button>
                         </div>

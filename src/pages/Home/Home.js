b4fdf669e210ce182970ce512ffd64ca604fdbfa
saputroandi <saputro.andi593@gmail.com>
2021-02-28 18:40:40 +0700

    add grid to the house

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -14,28 +14,20 @@ export default function Home(){
         <React.Fragment>
             {/* Navbar */}
             <nav className="static top-0 hidden lg:block bg-black">
-                <div className="flex py-3 mx-4">
-                    <div className=" flex w-full place-content-between">
-                        <div className="p-2 my-2 text-white uppercase">
-                            Ganti Kaos
-                        </div>
-                        <div className="flex flex-row">
-                            <div className=" lg:mr-4">
-                                <input type="text" placeholder="Search..." className=" border-gray-300 mt-2 h-10 px-5 pr-16 text-sm focus:outline-none" />
-                            </div>
+                <div className="container mx-auto grid grid-cols-5 gap-1 py-6">
+                    <div className="grid content-center text-white uppercase">
+                        Ganti Kaos
+                    </div>
+                    <div className="col-start-4">
+                        <div className=" justify-items-center">
+                            <input type="text" placeholder="Search..." className=" border-gray-300 h-10 w-full pl-2 text-sm focus:outline-none" />
                         </div>
                     </div>
-                    <div className="py-2 block mt-0">
-                        <div className=" flex flex-col lg:flex-row place-content-between  w-40 ml-8 mr-14 ">
-                            <div className="py-2 lg:py-0">
-                                <button className="mx-1 p-2 text-white uppercase">Menu</button>
-                            </div>
-                            <div className="py-2 lg:py-0">
-                                <button className="mx-1 p-2 text-white uppercase">Profile</button>
-                            </div>
-                            <div className="py-2 lg:py-0">
-                                <button className="mx-1 p-2 text-white uppercase">Cart</button>
-                            </div>
+                    <div className="col-start-5 col-end-6 py-2 block mt-0">
+                        <div className="grid grid-cols-3 justify-items-center">
+                            <button className=" text-white uppercase focus:outline-none">Menu</button>
+                            <button className=" text-white uppercase focus:outline-none">Wishlist</button>
+                            <button className=" text-white uppercase focus:outline-none">Cart</button>
                         </div>
                     </div>
                 </div>
@@ -44,7 +36,7 @@ export default function Home(){
 
             {/* Carousel */}
             <div className="max-w-screen-2xl max-h-80 m-auto">
-                <Carousel autoPlay infiniteLoop interval={2500} showStatus={false} showThumbs={false}>
+                <Carousel autoPlay infiniteLoop interval={2500} showStatus={false} showThumbs={false} centerMode={true} stopOnHover={true}>
                     <div>
                         <img className="w-full h-40 lg:h-80" src="https://images.unsplash.com/photo-1448375240586-882707db888b?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80"/>
                     </div>
@@ -59,7 +51,7 @@ export default function Home(){
             {/* End Carousel */}
 
             {/* Product */}
-            <main className="mb-20 mt-8 lg:my-8">
+            <main className="mb-16 mt-8 lg:my-8">
                 <div className="container mx-auto px-4 ">
                     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mt-6 ">
                         <div className="p-4 w-full max-w-sm mx-auto overflow-hidden border-2 border-white hover:border-black">
@@ -71,7 +63,7 @@ export default function Home(){
                                     <svg className="h-6 w-6" fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} viewBox="0 0 24 24" stroke="currentColor"><path d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" /></svg>
                                 </button>
                             </div>
-                            <div className=" py-3">
+                            <div className="px-4 py-3 bg-gray-100">
                                 <h3 className="text-gray-700 uppercase">Classic watch</h3>
                                 <hr></hr>
                                 <span className="text-gray-500 mt-2 flex justify-end">$123</span>
@@ -86,7 +78,7 @@ export default function Home(){
                                     <svg className="h-6 w-6" fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} viewBox="0 0 24 24" stroke="currentColor"><path d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" /></svg>
                                 </button>
                             </div>
-                            <div className="px-5 py-3">
+                            <div className="px-4 py-3 bg-gray-100">
                                 <h3 className="text-gray-700 uppercase">Old watch</h3>
                                 <hr></hr>
                                 <span className="text-gray-500 mt-2 flex justify-end">$95</span>
@@ -101,7 +93,7 @@ export default function Home(){
                                     <svg className="h-6 w-6" fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} viewBox="0 0 24 24" stroke="currentColor"><path d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" /></svg>
                                 </button>
                             </div>
-                            <div className="px-5 py-3">
+                            <div className="px-4 py-3 bg-gray-100">
                                 <h3 className="text-gray-700 uppercase">Classic watch</h3>
                                 <hr></hr>
                                 <span className="text-gray-500 mt-2 flex justify-end">$125</span>
@@ -116,7 +108,7 @@ export default function Home(){
                                     <svg className="h-6 w-6" fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} viewBox="0 0 24 24" stroke="currentColor"><path d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" /></svg>
                                 </button>
                             </div>
-                            <div className="px-5 py-3">
+                            <div className="px-4 py-3 bg-gray-100">
                                 <h3 className="text-gray-700 uppercase">fossil watch</h3>
                                 <hr></hr>
                                 <span className="text-gray-500 mt-2 flex justify-end">$180</span>
@@ -129,37 +121,31 @@ export default function Home(){
 
                 {/* mobile bottom navigation */}
                 <div className="fixed w-full py-2 bottom-0 lg:hidden bg-black">
-                    <div className="flex flex-row justify-between px-6 mx-4">
-                        <div className="flex-col">
-                            <button className=" focus:outline-none focus:ring-1 focus:ring-blue-600 focus:opacity-100 focus:shadow-none p-1">
-                                <div className="flex justify-center bg-black text-white">
-                                    <svg className="w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path></svg>
-                                </div>
-                                <div>
-                                    <p className="font-light text-xs text-white">Menu</p>
-                                </div>
+                    <div className=" grid grid-cols-3 justify-items-center">
+                        <button className="focus:outline-none p-1">
+                            <div className="flex justify-center bg-black text-white">
+                                <svg className="w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path></svg>
+                            </div>
+                            <div>
+                                <p className="font-light text-xs text-white focus:outline-none">Menu</p>
+                            </div>
+                        </button>
+                        <button className="focus:outline-none p-1">
+                            <div className="flex justify-center bg-black text-white">
+                                <svg className="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" /></svg>
+                            </div>
+                            <div>
+                                <p className="font-light text-xs text-white focus:outline-none">Wishlist</p>
+                            </div>
+                        </button>
+                        <button className="focus:outline-none p-1">
+                            <div className="flex justify-center bg-black text-white">
+                                <svg className="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" /></svg>
+                            </div>
+                            <div>
+                                <p className="font-light text-xs text-white focus:outline-none">Cart</p>
+                            </div>
                             </button>
-                        </div>
-                        <div className="flex-col">
-                            <button className=" focus:outline-none focus:ring-1 focus:ring-blue-600 focus:opacity-100 focus:shadow-none p-1">
-                                <div className="flex justify-center bg-black text-white">
-                                    <svg className="w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z" clipRule="evenodd"></path></svg>
-                                </div>
-                                <div>
-                                    <p className="font-light text-xs text-white">Profile</p>
-                                </div>
-                            </button>
-                        </div>
-                        <div className="flex-col">
-                            <button className=" focus:outline-none focus:ring-1 focus:ring-blue-600 focus:opacity-100 focus:shadow-none p-1">
-                                <div className="flex justify-center bg-black text-white">
-                                <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"></path></svg>
-                                </div>
-                                <div>
-                                    <p className="font-light text-xs text-white">Cart</p>
-                                </div>
-                            </button>
-                        </div>
                     </div>
                 </div>
                 {/* end mobile bottom navigation */}
--- src/pages/Product/Product.js
+++ src/pages/Product/Product.js
@@ -4,49 +4,72 @@ export default function Product() {
 
     return (
         <React.Fragment>
-            <div className="container my-8">
+            <div className="container my-8 mx-auto">
                 {/* product */}
-                <div className="flex flex-col mx-auto max-w-md lg:flex-row lg:max-w-full">
+                <div className="grid grid-cols-2 gap-10 items-center">
                     {/* product image */}
-                    <div className="w-full h-64 lg:h-96">
-                        <img className="h-full w-full rounded-md object-cover max-w-lg mx-auto" src="https://images.unsplash.com/photo-1578262825743-a4e402caab76?ixlib=rb-1.2.1&auto=format&fit=crop&w=1051&q=80" alt="Nike Air" />
+                    <div>
+                        <img className="h-full w-full object-cover max-w-lg mx-auto" src="https://images.unsplash.com/photo-1578262825743-a4e402caab76?ixlib=rb-1.2.1&auto=format&fit=crop&w=1051&q=80" alt="Nike Air" />
                     </div>
                     {/* order button */}
-                    <div className="w-full max-w-lg justify-center my-16">
+                    <div className="grid grid-rows-1 gap-y-2">
                         <h3 className="text-gray-700 uppercase text-lg">Nike Air</h3>
-                        <span className="text-gray-500 mt-3">$125</span>
-                        <hr className="my-3" />
-                        <div className="mt-2">
-                            <label className="text-gray-700 text-sm" htmlFor="count">Count:</label>
-                            <div className="flex items-center mt-1">
+                        <span className="text-gray-500">Rp. 1.000.000,-</span>
+                        <div>
+                            <label className="text-gray-700 text-sm" htmlFor="count">Jumlah:</label>
+                            <div className="flex gap-x-1 justify-items-center">
                                 <button className="text-gray-500 focus:outline-none focus:text-gray-600">
                                     <svg className="h-5 w-5" fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} viewBox="0 0 24 24" stroke="currentColor"><path d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
                                 </button>
-                                <span className="text-gray-700 text-lg mx-2">20</span>
+                                <span className="text-gray-700 text-lg">20</span>
                                 <button className="text-gray-500 focus:outline-none focus:text-gray-600">
                                     <svg className="h-5 w-5" fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} viewBox="0 0 24 24" stroke="currentColor"><path d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
                                 </button>
                             </div>
                         </div>
-                        <div className="mt-3">
-                            <label className="text-gray-700 text-sm" htmlFor="count">Color:</label>
-                            <div className="flex items-center mt-1">
-                                <button className="h-5 w-5 rounded-full bg-blue-600 border-2 border-blue-200 mr-2 focus:outline-none" />
-                                <button className="h-5 w-5 rounded-full bg-teal-600 mr-2 focus:outline-none" />
-                                <button className="h-5 w-5 rounded-full bg-pink-600 mr-2 focus:outline-none" />
+                        <div>
+                            <label className="text-gray-700 text-sm" htmlFor="count">Size:</label>
+                            <div className="flex justify-items-center">
+                                <button className="text-gray-500 focus:outline-none focus:text-gray-600">
+                                    <svg className="h-5 w-5" fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} viewBox="0 0 24 24" stroke="currentColor"><path d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
+                                </button>
+                                <span className="text-gray-700 text-lg mx-2">L</span>
+                                <button className="text-gray-500 focus:outline-none focus:text-gray-600">
+                                    <svg className="h-5 w-5" fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} viewBox="0 0 24 24" stroke="currentColor"><path d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
+                                </button>
                             </div>
                         </div>
-                        <div className="flex items-center mt-6">
-                            <button className="px-8 py-2 bg-indigo-600 text-white text-sm font-medium rounded hover:bg-indigo-500 focus:outline-none focus:bg-indigo-500">Order Now</button>
-                            <button className="mx-2 text-gray-600 border rounded-md p-2 hover:bg-gray-200 focus:outline-none">
-                            <svg className="h-5 w-5" fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} viewBox="0 0 24 24" stroke="currentColor"><path d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" /></svg>
-                            </button>
+                        <div>
+                            <label className="text-gray-700 text-sm" htmlFor="count">Warna:</label>
+                            <div className="flex gap-x-1 justify-items-center">
+                                <button className="h-5 w-5 bg-blue-600" />
+                                <button className="h-5 w-5 bg-yellow-600" />
+                                <button className="h-5 w-5 bg-pink-600" />
+                            </div>
+                        </div>
+                        <div className="grid grid-cols-1 w-56 gap-y-2">
+                            <div className="grid grid-cols-5 justify-items-center gap-x-1">
+                                <button className="col-start-1 col-end-5 w-full bg-black text-white text-xs focus:outline-none">Tambah Ke Wishlist</button>
+                                <button className="flex justify-center w-full text-gray-600 border hover:bg-gray-200 focus:outline-none">
+                                    <svg className="h-6 w-6" fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} viewBox="0 0 24 24" stroke="currentColor"><path d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" /></svg>
+                                </button>
+                            </div>
+                            <div>
+                                <button className="w-full py-3 bg-black text-white focus:outline-none">Beli Sekarang</button>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-            <div>
-                {/* info product */}
+            {/* details product */}
+            <div className="container my-8 mx-auto">
+                <div className="w-full" style={{borderBottom: '1px solid #eaeaea'}}>
+                    <ul className="flex cursor-pointer space-x-3">
+                        <li className="py-2 px-6">Deskripsi</li>
+                        <li className="py-2 px-6">Kebijakan</li>
+                        <li className="py-2 px-6">Review</li>
+                    </ul>
+                </div>
             </div>
         </React.Fragment>
     )
